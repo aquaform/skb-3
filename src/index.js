2ee3d70f6dbc746cf828cebacd932d61cbaf8c30
aquaform <aquaform@yandex.ru>
2016-11-09 15:42:58 +0500

    Первый коммит

--- src/index.js
+++ src/index.js
@@ -3,44 +3,28 @@ import cors from 'cors';
 
 const app = express();
 
-function tranformFIO(prmArray) {	
-	const badmsg = 'Invalid fullname';
+function calculate(query) {	
+	const reA = /((\-)?\d+(\.\d+)?)/;
 	try {
-		var re = /^([A-ZА-ЯЁ][a-zа-яёó]+)( [A-ZА-ЯЁ][a-zа-яёó]+)?( [A-ZА-ЯЁ][a-zа-яёó]+)?$/;
-		var abbrFIO = prmArray['fullname'].match(re);
-		var name1 = ''; 
-		var name2 = '';
-		var name3 = '';
-		if (!!abbrFIO[3]) {
-			name1 = abbrFIO[3].replace(/^\s*/,'').replace(/\s*$/,'');	
-			name2 = ' ' + abbrFIO[1].replace(/^\s*/,'').replace(/\s*$/,'')[0] + '.';
-			name3 = ' ' + abbrFIO[2].replace(/^\s*/,'').replace(/\s*$/,'')[0] + '.'
-		} else if (!!abbrFIO[2]) {
-			name1 = abbrFIO[2].replace(/^\s*/,'').replace(/\s*$/,'');
-			name2 = ' ' +abbrFIO[1].replace(/^\s*/,'').replace(/\s*$/,'')[0] + '.';			
-		} else {
-			name1 = abbrFIO[1].replace(/^\s*/,'').replace(/\s*$/,'');		
-		}
-		
-
-		abbrFIO = name1 + name2 + name3;
-
+		var a = query['a'].match(reA)[1];
+	} catch(e) {		
+		return 0;
+	}		
+	const reB = /((\-)?\d+(\.\d+)?)/;	
+	try {
+		var b = (query['b'].match(reB)[1]);
 	} catch (e) {
-		return badmsg;
+		return a;
 	}
-	
-	return abbrFIO;
-}
-
-
+	return Number(a) + Number(b);	
+};
 
 app.use(cors());
-app.get('/task2b', (req, res) => {
-  
-  res.send(tranformFIO(req.query));
-
-
+app.get('/task2a', (req, res) => {	
+	var sum = (+req.query.a || 0) + (+req.query.b || 0);
+	res.send(''+sum);
 });
+
 
 app.listen(3000, () => {
   console.log('Your app listening on port 3000!');
